b232c6a68a2db8a23560f2a8d97b39ba744ac939
HafidhRizky <hafidhrzk@gmail.com>
2023-05-12 09:58:31 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import "./index.css";
 
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from "react-query";
 const client = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
+import React from "react";
 
-
-export default function Home (){
-    return "Ini Home"
+export default function Home() {
+  return <></>;
 }
